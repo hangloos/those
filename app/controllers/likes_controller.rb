d04b562f63f23b5a0730616684722ab395c5e356
Eric Loos <ericloos00@gmail.com>
2017-01-28 16:11:27 -0600

    create and delete like for a comment work on the same button. first click will create, second will delete

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,27 +4,17 @@ class LikesController < ActionController::Base
 
     def create
         comment = Comment.find_by_id(params[:comment])
-        l = Like.where("user_id = ? AND comment_id =?", current_user.id,comment.id)
-        like = comment.likes.build(user_id: current_user.id)
-          if !l[0] && like.save
+        if like = Like.find_by(user_id: current_user.id, comment_id: comment.id)
+          like.delete
+          comment.save
+        else
+          like = Like.create(user_id: current_user.id, comment_id: comment.id)
+        end
+          if like.save
               render json: { status: 'ok'}
           else
-              flash[:error] = 'You already likes this comment'
               render json: { errors: like.errors.full_messages },  status: :unprocessable_entity
           end
     end
-
-    def destroy
-      binding.pry
-      like = like.find_by_id(params[:like][:id])
-            if like.user == current_user
-            like.delete
-            render json: { status: 'ok'}
-            else
-            render json: { errors: like.errors.full_messages }, status: :unprocessable_entity
-            end
-    end
-
-
 
 end
