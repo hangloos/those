8ec98f535d840cec4f31871feb116a94f2b66443
Eric Loos <ericloos00@gmail.com>
2017-01-30 13:38:37 -0600

    changed size and style of add comment input

--- app/assets/javascripts/review/review.controller.js
+++ app/assets/javascripts/review/review.controller.js
@@ -230,7 +230,7 @@
 
     app.filter('startFrom', function() {
     return function(input, start) {
-        start = +start; //parse to int
+        start = start++; //parse to int
         return input.slice(start);
     }
 });
