1eb986f2ca515fe70c71d0d8f2a0772026de2c04
Eric Loos <ericloos00@gmail.com>
2017-01-30 20:22:33 -0600

    deleted serialzers that arent needed. Added review type on reviews page

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,10 +1,10 @@
-class CommentSerializer < ActiveModel::Serializer 
-    attributes :id, :user_id, :comment, :review_id, :created_at, :review
+ class CommentSerializer < ActiveModel::Serializer 
+     attributes :id, :user_id, :comment, :review_id, :created_at, :review
 
-    has_one :review
-    has_one :user
-    has_many :likes
+     has_one :review
+     has_one :user
+     has_many :likes
 
     
 
-end
+ end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer 
   
-   attributes :id,:awards, :actors, :review_likes, :genres, :box_office, :director, :language, :picture_url, :production, :rated, :year, :runtime, :title, :review_type, :imdb_id, :viewing_platform, :those_movie_guys_rating, :those_movie_guys_review, :writer, :imdb_rating, :imdb_votes, :tomato_consensus_review, :tomato_user, :tomato_user_votes,  :tomato_critics, :tomato_critics_votes, :tomato_url, :created_at, :updated_at, :user_id
+   attributes :id,:actors, :review_likes, :genres, :director, :picture_url, :runtime, :title, :review_type, :imdb_id, :viewing_platform, :those_movie_guys_rating, :those_movie_guys_review, :imdb_rating, :imdb_votes, :tomato_user, :tomato_user_votes,  :tomato_critics, :tomato_critics_votes, :tomato_url, :created_at, :updated_at, :user_id
 
    has_many :comments
    has_one :user
