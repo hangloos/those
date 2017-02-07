faf274eb85e3b7b2584f8399b1bdebc0507010ad
Eric Loos <ericloos00@gmail.com>
2017-02-07 11:36:52 -0600

    fixed likes on comments

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,20 +1,19 @@
 class LikesController < ActionController::Base
 
-    before_action :authenticate_user!, only: [:create, :delete]
+    before_action :authenticate_user!, only: [:create]
 
     def create
         comment = Comment.find_by_id(params[:comment])
-        if like = Like.find_by(user_id: current_user.id, comment_id: comment.id)
+        like = Like.find_by(user_id: current_user.id, comment_id: comment.id)
+        if !!like
           like.delete
           comment.save
+          render json: { status: 'ok'}
         else
           like = Like.create(user_id: current_user.id, comment_id: comment.id)
+          comment.save
+          render json: { status: 'ok'}
         end
-          if like.save
-              render json: { status: 'ok'}
-          else
-              render json: { errors: like.errors.full_messages },  status: :unprocessable_entity
-          end
     end
 
 end
